0337661d93bfde2e3a33b2376e9426669b676719
Laurent Cheylus <foxy@free.fr>
2025-08-20 15:06:57 +0200

    utils/strings: add function to convert an Unicode string to standard ASCII
    Open and personal messages for ICB protocol support only standard ASCII
    (numeric code for char < 127). Before sending a message to an ICB server,
    need to convert Unicode input to ASCII.
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module irc2icb
 
 go 1.24.4
 
-require github.com/BurntSushi/toml v1.5.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.5.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+)
--- utils/strings.go
+++ utils/strings.go
@@ -4,6 +4,9 @@ package utils
 
 import (
 	"strings"
+	"unicode"
+
+	"golang.org/x/text/unicode/norm"
 )
 
 // Check if string is a valid IRC channel (starts with '#')
@@ -31,4 +34,46 @@ func CompareUser(user1, user2 string) bool {
 		return true
 	}
 	return false
+}
+
+// Custom map for specific case of conversion from Unicode to ASCII string
+var translateAsciiMap = map[rune]string{
+	'ß': "ss",
+	'ø': "o",
+	'Ø': "O",
+	'æ': "ae",
+	'Æ': "AE",
+	'œ': "oe",
+	'Œ': "OE",
+}
+
+// TransliterateUnicodeToASCII converts a Unicode string to pure ASCII
+// Diacritics marks are skipped and some chars are translated via a custom map.
+func TransliterateUnicodeToASCII(input string) string {
+	// Normalize the string to decomposed form (NFD)
+	normStr := norm.NFD.String(input)
+	result := make([]rune, 0, len(normStr))
+
+	for _, r := range normStr {
+		// Not a standard ASCII character
+		if r > 127 {
+			// Skip diacritics
+			if unicode.Is(unicode.Mn, r) {
+				continue
+			}
+			if repl, ok := translateAsciiMap[r]; ok {
+				for _, replRune := range repl {
+					result = append(result, replRune)
+				}
+			}
+			// For non latin char, replace by *
+			if !unicode.Is(unicode.Latin, r) {
+				result = append(result, rune('*'))
+			}
+			continue
+		}
+		result = append(result, r)
+	}
+
+	return string(result)
 }
