826708c073f56e1a9f8b1c76a88f6ca9292b974e
Laurent Cheylus <foxy@free.fr>
2025-08-25 17:44:28 +0200

    Split main code to handle IRC LIST command
    - Add irc_command.go source file for functions to handle IRC commands.
    - Move code from main.go to irc_commands.go to handle IRC LIST command.
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"os/exec"
 	"os/signal"
 	"sort"
-	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -250,82 +249,7 @@ func handleIRCConnection(irc_conn net.Conn, server_addr string, server_port int)
 			}
 
 		case irc.IrcCommandList:
-			// TODO Add cache with duration => not query ICB server for
-			// groups/users for each LIST command
-
-			// Filter channels/groups with IRC command "LIST" paramaters
-			var query string
-			if len(params) == 0 {
-				query = ""
-			} else {
-				query = params[0]
-			}
-			valid_channels, invalid_channels, err := irc.IrcFilterList(query)
-			if err != nil {
-				irc.IrcSendRaw(irc_conn, "ERROR :%s", err.Error())
-				irc.IrcSendCode(irc_conn, irc.IrcNick, irc.IrcReplyCodes["RPL_LISTEND"], ":End of /LIST")
-				break
-			}
-
-			logger.LogInfo("LIST command => send ICB command to get groups and users")
-			icb.IcbQueryGroupsUsers(icb_conn, true)
-
-			var valid_groups []*icb.IcbGroup
-
-			// LIST command for all channels
-			if len(valid_channels) == 0 && len(invalid_channels) == 0 {
-				for _, group := range icb.IcbGroups {
-					valid_groups = append(valid_groups, group)
-				}
-				goto SendListReplies
-			}
-
-			// LIST request ">N" => return groups with more than N users
-			if len(valid_channels) == 1 && strings.HasPrefix(valid_channels[0], ">") {
-				n_users, _ := strconv.Atoi(valid_channels[0][1:])
-				for _, group := range icb.IcbGroups {
-					if len(group.Users) >= n_users {
-						valid_groups = append(valid_groups, group)
-					}
-				}
-				goto SendListReplies
-			}
-
-			// LIST command to filter channels with pattern ('#chan*')
-			if len(valid_channels) == 1 && strings.HasSuffix(valid_channels[0], "*") {
-				prefix := strings.Trim(valid_channels[0], "*")
-				for _, group := range icb.IcbGroups {
-					if strings.HasPrefix(group.Name, utils.GroupFromChannel(prefix)) {
-						valid_groups = append(valid_groups, group)
-					}
-				}
-				goto SendListReplies
-			}
-
-			for _, irc_channel := range valid_channels {
-				group := icb.IcbGetGroup(utils.GroupFromChannel(irc_channel))
-				if group != nil {
-					valid_groups = append(valid_groups, group)
-				} else {
-					irc.IrcSendNotice(irc_conn, "*** :Unknown ICB group '%s' in LIST command", utils.GroupFromChannel(irc_channel))
-				}
-			}
-			goto SendListReplies
-
-		SendListReplies:
-			logger.LogDebugf("Send reply to LIST command - nick = %s", irc.IrcNick)
-
-			// Send IRC numeric reply RPL_LIST for each valid group
-			for _, group := range valid_groups {
-				logger.LogDebugf("[Group] Name = %s - Topic = '%s' - %d users %q", group.Name, group.Topic, len(group.Users), group.Users)
-				irc.IrcSendCode(irc_conn, irc.IrcNick, irc.IrcReplyCodes["RPL_LIST"], "%s %d :%s", utils.GroupToChannel(group.Name), len(group.Users), group.Topic)
-			}
-			irc.IrcSendCode(irc_conn, irc.IrcNick, irc.IrcReplyCodes["RPL_LISTEND"], ":End of /LIST")
-
-			// Send IRC notice for each invalid channel
-			for _, irc_channel := range invalid_channels {
-				irc.IrcSendNotice(irc_conn, "*** :Invalid channel '%s' in LIST command", irc_channel)
-			}
+			ircCommandList(irc_conn, icb_conn, params)
 
 		case irc.IrcCommandNames:
 			logger.LogInfof("NAMES command => paramaters = %s", params[0])
